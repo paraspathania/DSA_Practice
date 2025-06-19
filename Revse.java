8896fc29e11ecb6da22b873b461490828ba706ba
paraspathania <paraspathania705@gmail.com>
2025-06-19 23:03:27 +0530

    updated

--- Revse.java
+++ Revse.java
@@ -1,29 +1,25 @@
+// Given an integer num, reverse the number and return the answer using different approaches.
 
+// approach 1: Using a while loop to reverse the number,
 import java.util.Scanner;
  public class Revse {
-		public static void main(String[] args) {
-		Scanner d = new Scanner(System.in);
-		System.out.println("Enter a number to reverse:");
-		int n = d.nextInt();
-		int reversed = 0;
+		// public static void main(String[] args) {
+		// Scanner d = new Scanner(System.in);
+		// System.out.println("Enter a number to reverse:");
+		// int n = d.nextInt();
+		// int reversed = 0;
 
-		while(n != 0){
-			int num = n % 10;
-			reversed = reversed * 10 + num;
-			n = n / 10; 
-		}
-		System.out.println("Reversed number is:" + reversed);
-		d.close();
-		}
-	}
+		// while(n != 0){
+		// 	int num = n % 10;
+		// 	reversed = reversed * 10 + num;
+		// 	n = n / 10; 
+		// }
+		// System.out.println("Reversed number is:" + reversed);
+		// d.close();
+		// }
 
+// appraoch 2: using an iterative method to reverse the number.
 
-
-
-////                   ****  Iterative approach for reverseing the numbers. **********
- 
-// import java.util.Scanner;
-// class Revse{
 // 	public static int reverse(int num){
 // 		int reversed = 0;
 // 		while(num != 0){
@@ -38,9 +34,7 @@ import java.util.Scanner;
 // 			int num = d.nextInt();
 // 			System.out. println("Reversed number is: " + reverse(num));
 // 		}
-	
-// }
 
+// approach 3: using recursion to reverse the number.
+	}
 
-
-/// 
