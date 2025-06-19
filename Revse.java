a6655f83deab9c0631529c00096dcccfaa2504eb
paraspathania <paraspathania705@gmail.com>
2025-06-19 22:32:51 +0530

    summer_training_DSA

---
+++ Revse.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+ public class Revse {
+		public static void main(String[] args) {
+		Scanner d = new Scanner(System.in);
+		System.out.println("Enter a number to reverse:");
+		int n = d.nextInt();
+		int reversed = 0;
+
+		while(n != 0){
+			int num = n % 10;
+			reversed = reversed * 10 + num;
+			n = n / 10; 
+		}
+		System.out.println("Reversed number is:" + reversed);
+		d.close();
+		}
+	}
+
+
+
+
+////                   ****  Iterative approach for reverseing the numbers. **********
+ 
+// import java.util.Scanner;
+// class Revse{
+// 	public static int reverse(int num){
+// 		int reversed = 0;
+// 		while(num != 0){
+// 			int digit = num % 10;
+// 			reversed = reversed * 10 + digit;
+// 			num = num / 10; 
+// 		}
+// 		return reversed;
+// 	}
+// 		public static void main (String [] args){
+// 			Scanner d = new Scanner(System.in);
+// 			int num = d.nextInt();
+// 			System.out. println("Reversed number is: " + reverse(num));
+// 		}
+	
+// }
+
+
+
+/// 
