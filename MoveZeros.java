5eba0ecd94a66f227ef29bdb079d16adffce4a85
paraspathania <paraspathania705@gmail.com>
2025-07-05 23:16:25 +0530

    Move zeros at the end of an array by using two-pointer approach

---
+++ MoveZeros.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+public class MoveZeros {
+    // This method moves all zeros in the array to the end while maintaining the order of non-zero elements.
+    // It uses a two-pointer technique where one pointer tracks the position of the next non-zero element.
+       public static void moveZerosToEnd(int[] arr) {
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[nonZeroIndex];
+                arr[nonZeroIndex] = temp;
+                nonZeroIndex++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,0,78,0,56,23,0,85,0};
+	
+	System.out.print("Elements before move all the zeros: ");
+	for (int num1 : arr){
+	System.out.print(num1 + " ");
+	}
+    System.out.println();
+    System.out.println("Moving all zeros to the end of the array...");
+        moveZerosToEnd(arr);
+
+        System.out.print("Result: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
