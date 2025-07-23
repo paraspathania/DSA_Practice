0ef1c765899d8cb3729b4a3cd5f48809fdfa09df
paraspathania <paraspathania705@gmail.com>
2025-07-23 22:46:43 +0530

    binary search

--- cpp/binarySearch.cpp
+++ cpp/binarySearch.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int binartSearch(int arr [], int n, int t){
-    while()
+// int binartSearch(int arr [], int n, int t){
+    
+// }
+
+int main(){
+    int n, t;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: "<<endl;
+    for(int i =0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter the element to be searched: ";
+    cin>>t;
+
+    cout<<"The size of an array is: "<<sizeof(arr)/sizeof(arr[0]);
 }
